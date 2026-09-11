468754df932f3a4200ec783a2f02d8587419f9b6
ayemteezy <laurencelestercarino@gmail.com>
2026-09-11 12:18:31 +0800

    fix: remove unnecessary padding and empty line in RouteComponent for cleaner layout

--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -20,7 +20,7 @@ function RouteComponent() {
 		<main
 			aria-hidden={isOpen}
 			inert={isOpen}
-			className="flex min-h-full flex-col pb-20 sm:px-12 lg:px-0"
+			className="flex min-h-full flex-col sm:px-12 lg:px-0"
 		>
 			<div className="container flex flex-col space-y-21 pt-12 pb-12">
 				<div className="flex flex-col gap-6 md:gap-8">
@@ -45,7 +45,6 @@ function RouteComponent() {
 				<Telemetry />
 
 				{/* Footer */}
-
 				<Footer />
 			</div>
 		</main>
