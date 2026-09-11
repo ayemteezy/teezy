5b5ceab3806020de09444f128506815442e66b66
ayemteezy <laurencelestercarino@gmail.com>
2026-09-11 16:04:41 +0800

    fix: standardize padding in Route components for consistent layout

--- src/routes/_app/stack/index.tsx
+++ src/routes/_app/stack/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/_app/stack/")({
 
 function RouteComponent() {
 	return (
-		<div className="container space-y-6 py-6">
+		<div className="container space-y-6 pt-6 pb-6 lg:pt-12">
 			<PageHeader />
 			<ComingSoon />
 		</div>
